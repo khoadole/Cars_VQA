ba16ddb6fea04277b2b67ebb413f254912633971
khoadole <dolekhoa1407@gmail.com>
2025-06-15 15:21:02 +0700

    vendor: change port

--- app/app.py
+++ app/app.py
@@ -46,4 +46,5 @@ def handle_upload():
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
+    print(f"Starting Flask on port: {port}")
     app.run(host='0.0.0.0', port=port, debug=False)
